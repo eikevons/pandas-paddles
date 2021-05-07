40e1b3b0ab3a405222455ea34a430f1fbea8f8e8
Eike von Seggern <eike.von.seggern@dainstudios.com>
2021-05-07 09:24:06 +0200

    Fix doc title

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 
 # -- Project information -----------------------------------------------------
 
-project = "Pandas Accessors"
+project = "Pandas Selector"
 author = "Eike von Seggern"
 copyright = f"2021, {author}"
 
