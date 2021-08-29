18f07026dd5975f88ac81a4916427a46b0248a91
eikevons <eikevons@yahoo.de>
2021-08-29 12:51:12 +0200

    Wrap function args (#4)
    * Wrap all method arguments not only the first argument
    
    This allows code like this::
    >>> df.assign(x_cliped_by_y_min = DF["x"].clip(DF["y"].min())
    
    * Improve str for method accessor: include all arguments
    
    * More examples in readme
    
    Co-authored-by: Eike von Seggern <eikevons@mailbox.org>

--- tests/test_str.py
+++ tests/test_str.py
@@ -1,6 +1,8 @@
+from pandas_selector.df_accessor import AccessorBase
 from pandas_selector import DF, S
 
 
+# DataFrame accessor formatting
 def test_attribute_str():
     assert ".a" == str(DF.a)
 
@@ -20,16 +22,26 @@ def test_item_attribute_str():
     assert "['a'].BB" == str(DF["a"].BB)
 
 def test_operator_str():
-    assert ".x.__lt__(...)" == str(DF.x < None)
+    assert ".x.__lt__(9)" == str(DF.x < 9)
 
 def test_column_method_no_args_str():
     assert ".x.abs()" == str(DF.x.abs())
 
-def test_column_method_with_args_str():
-    assert ".x.isin(...)" == str(DF.x.isin({None}))
+def test_column_method_with_arg_str():
+    assert ".x.isin({1, 2})" == str(DF.x.isin({1, 2}))
 
+def test_column_method_with_args_str():
+    assert ".x.clip(1, 2)" == str(DF.x.clip(1, 2))
+
+def test_column_method_with_arg_kwarg_str():
+    assert ".x.clip(1, upper=2)" == str(DF.x.clip(1, upper=2))
+
+def test_method_with_nested_args_str():
+    assert ".x.method_name(.y.min())" == str(DF.x.method_name(DF.y.min()))
+
+# Series accessor formatting
 def test_series_operator_str():
-    assert ".__lt__(...)" == str(S < None)
+    assert ".__lt__(8)" == str(S < 8)
 
 def test_series_attribute_str():
     assert ".index" == str(S.index)
@@ -37,5 +49,14 @@ def test_series_attribute_str():
 def test_series_method_no_args_str():
     assert ".abs()" == str(S.abs())
 
+def test_series_method_with_arg_str():
+    assert ".isin({3, 4})" == str(S.isin({3, 4}))
+
 def test_series_method_with_args_str():
-    assert ".isin(...)" == str(S.isin({None}))
+    assert ".meth_name(3, 'a')" == str(S.meth_name(3, 'a'))
+
+def test_series_method_with_kwarg_str():
+    assert ".clip(lower=1)" == str(S.clip(lower=1))
+
+def test_series_method_with_arg_kwarg_str():
+    assert ".clip(1, upper=2)" == str(S.clip(1, upper=2))
