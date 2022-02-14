7d268d569235c5fa9ba09738b10653df48b2f891
Jan Eike von Seggern <eikevons@mailbox.org>
2022-02-14 23:08:44 +0100

    Add selecting by multi-index levels

--- tests/test_select_axis.py
+++ tests/test_select_axis.py
@@ -7,50 +7,113 @@ from pandas_selector.axis import C, OpComposerBase
 
 def cols(df: pd.DataFrame, col_sel: OpComposerBase) -> list:
     """Get list of column names after applying column selection."""
-    return df.loc[:,col_sel].columns.to_list()
+    return df.loc[:, col_sel].columns.to_list()
 
 
 @pytest.fixture
 def simple_df():
-    return pd.DataFrame({
-        "x": [1,2,3,4,5],
-        "y": [1,2,5,5,4],
-        "z": list("abcde"),
-        })
+    return pd.DataFrame(
+        {
+            "x": [1, 2, 3, 4, 5],
+            "y": [1, 2, 5, 5, 4],
+            "z": list("abcde"),
+        }
+    )
+
+
+@pytest.fixture
+def mi_df():
+    return pd.DataFrame(
+        columns=pd.MultiIndex.from_product(
+            [list("abc"), list("XYZ")],
+            names=["one", "two"],
+        )
+    )
 
 
 def test_basic(simple_df):
     col_sel = C["y", "z", "x"]
-    assert ["y", "z", "x"] == cols(simple_df, col_sel)
+    assert cols(simple_df, col_sel) == ["y", "z", "x"]
 
 
 def test_combine(simple_df):
     col_sel = C["y"] | C["z"] | C["x"]
-    assert (["y", "z", "x"]
-            == simple_df.loc[:, col_sel].columns.to_list())
+    assert cols(simple_df, col_sel) == ["y", "z", "x"]
 
 
 def test_ellipsis(simple_df):
     col_sel = C["y"] | ...
-    assert (["y", "x", "z"]
-            == simple_df.loc[:, col_sel].columns.to_list())
+    assert cols(simple_df, col_sel) == ["y", "x", "z"]
 
 
 def test_startswith(simple_df):
     col_sel = C.startswith("x")
-    assert ["x"] == simple_df.loc[:, col_sel].columns.to_list() == ["x"]
+    assert cols(simple_df, col_sel) == ["x"]
 
 
 def test_endswith(simple_df):
     col_sel = C.endswith("x")
-    assert ["x"] == cols(simple_df, col_sel)
+    assert cols(simple_df, col_sel) == ["x"]
 
 
 def test_str_dtype(simple_df):
     col_sel = C.dtype == str
-    assert ["z"] == cols(simple_df, col_sel)
+    assert cols(simple_df, col_sel) == ["z"]
 
 
 def test_int_dtype(simple_df):
     col_sel = C.dtype == int
-    assert ["x", "y"] == cols(simple_df, col_sel)
+    assert cols(simple_df, col_sel) == ["x", "y"]
+
+
+def test_level0_subset(mi_df):
+    expected = [
+        ("c", "X"),
+        ("c", "Y"),
+        ("c", "Z"),
+        ("a", "X"),
+        ("a", "Y"),
+        ("a", "Z"),
+    ]
+
+    col_sel = C.levels[0]["c", "a"]
+    assert cols(mi_df, col_sel) == expected
+
+    col_sel = C.levels["one"]["c", "a"]
+    assert cols(mi_df, col_sel) == expected
+
+
+def test_level0_str_methods(mi_df):
+    expected = [
+        ("c", "X"),
+        ("c", "Y"),
+        ("c", "Z"),
+    ]
+
+    col_sel = C.levels[0].startswith("c")
+    assert cols(mi_df, col_sel) == expected
+
+    col_sel = C.levels[0].endswith("c")
+    assert cols(mi_df, col_sel) == expected
+
+    col_sel = C.levels[0].contains("c")
+    assert cols(mi_df, col_sel) == expected
+
+    col_sel = C.levels[0].match("c")
+    assert cols(mi_df, col_sel) == expected
+
+
+def test_level0_composition(mi_df):
+    expected = [
+        ("c", "X"),
+        ("c", "Y"),
+        ("c", "Z"),
+        ("a", "X"),
+        ("a", "Y"),
+        ("a", "Z"),
+        ("b", "X"),
+        ("b", "Y"),
+        ("b", "Z"),
+    ]
+    col_sel = C.levels[0]['c'] | ...
+    assert cols(mi_df, col_sel) == expected
