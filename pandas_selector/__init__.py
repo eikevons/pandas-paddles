5096ef7af2ea8079598c305cc8069c407d091da2
Jan Eike von Seggern <eikevons@mailbox.org>
2022-02-19 19:16:55 +0100

    Release v1.2.1

--- pandas_selector/__init__.py
+++ pandas_selector/__init__.py
@@ -189,7 +189,7 @@ code::
 
 Author: Eike von Seggern <eike@vonseggern.space>
 """
-__version__ = "1.2.0-dev"
+__version__ = "1.2.1"
 __all__ = ["DF", "S"]
 
 from .df_accessor import DataframeAccessor, SeriesAccessor
