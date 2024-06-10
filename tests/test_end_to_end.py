192ff0ee4a7fe93c91624ed19c70805d03ddc610
Eike von Seggern <eikevons@mailbox.org>
2024-06-10 22:31:34 +0200

    Add first test-case
    This new test runs at least with pandas >= 2

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -3,6 +3,13 @@ from pandas_paddles import DF, S
 import pandas as pd
 import pytest
 
+HAS_DASK = False
+try:
+    import dask.dataframe
+    HAS_DASK = True
+except ImportError:
+    pass
+
 @pytest.fixture
 def df():
     return pd.DataFrame({
@@ -15,20 +22,41 @@ def ser():
     return pd.Series(range(5))
 
 
-def test_DF_end_to_end(df):
-    test = (df
+def ops(df):
+    return (df
             .assign(
                 y = DF.x // 2,
                 z = DF.x * DF.y,
                 )
             .loc[DF.y <= 1]
             )
+
+
+def test_DF_end_to_end(df):
+    test = ops(df)
     expected = pd.DataFrame({
         "x": [0, 1, 2, 3],
         "y": [0, 0, 1, 1],
         "z": [0, 0, 2, 3],
     })
     return pd.testing.assert_frame_equal(test, expected)
+
+@pytest.mark.skipif(not HAS_DASK, reason="dask not available")
+def test_DF_end_to_end_dask(df):
+    dask_df = dask.dataframe.from_pandas(df, chunksize=2, name="test-df")
+    dask_test = ops(dask_df)
+    pd_test = dask_test.compute()
+    expected = pd.DataFrame({
+        "x": [0, 1, 2, 3],
+        "y": [0, 0, 1, 1],
+        "z": [0, 0, 2, 3],
+    })
+    return pd.testing.assert_frame_equal(pd_test, expected)
+
+
+
+
+
 
 
 def test_S_end_to_end(ser):
